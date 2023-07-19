6b84ef3bc666889aa302c0e357386b4752d6d27b
Daniel <86194608+rejsek@users.noreply.github.com>
2023-07-19 18:59:26 +0200

    Sign can be convert

--- main.asm
+++ main.asm
@@ -5,24 +5,40 @@ ExitProcess proto,dwExitCode:dword
 
 
 .data
-    input           dd  20000     ; Inicializace celeho cisla pro prevod
+    input           dd  -1256000     ; Inicializace celeho cisla pro prevod
     msb_pos         dd  0       ; Inicializace pomocne promenne pro ulozeni hlavniho bitu
     result          dd  0       ; Inicializace promenne, do ktere se ulozi vysledny prevod
+    sign            dd  0
 
 .code
 main proc c
       ; Inicializace
-      xor eax, eax ; eax - vynulov·nÌ registru
-      xor ebx, ebx ; ebx - vynulov·nÌ registru
-      xor ecx, ecx ; ecx - vynulov·nÌ registru
-      xor edx, edx ; edx - vynulov·nÌ registru
-      xor esi, esi ; esi - vynulov·nÌ registru
+      xor eax, eax ; eax - vynulov√°n√≠ registru
+      xor ebx, ebx ; ebx - vynulov√°n√≠ registru
+      xor ecx, ecx ; ecx - vynulov√°n√≠ registru
+      xor edx, edx ; edx - vynulov√°n√≠ registru
+      xor esi, esi ; esi - vynulov√°n√≠ registru
 
       ; Nacteni vstupu
       mov eax, input
+      
+      mov ebx, input
+      shr ebx, 31
+
+      cmp ebx, 1        ; zjisti, zda je na ebx 1
+      jne continue
+      not eax
+      add eax, 1
+      mov ebx, 1
+      shl ebx, 31
+      mov[sign], ebx
+
+      continue:
+
+      xor ebx, ebx
 
       ; Hledani pozice nejvyznamnejsiho bity (MSB)
-      mov ecx, 31           ; Inicializace ËÌtaËe
+       mov ecx, 31           ; Inicializace ƒç√≠taƒçe
       mov edx, 80000000h    ; inicializave porovnavaci hodnoty
 
       search_msb:
@@ -37,12 +53,12 @@ main proc c
 
       ; Odstraneni nejvyznamnejsiho bitu
       mov ebx, eax
-      btr ebx, ecx       ; OdstranÏnÌ bitu na pozici MSB
+      btr ebx, ecx       ; Odstranƒõn√≠ bitu na pozici MSB
       mov eax, ebx
 
       ; Vypocet exponentu
-      mov esi, [msb_pos]    ; NaËtenÌ pozice MSB do registru ESI
-      add esi, 127          ; P¯iËtenÌ 127 k pozici MSB pro v˝poËet exponentu
+      mov esi, [msb_pos]    ; Naƒçten√≠ pozice MSB do registru ESI
+      add esi, 127          ; P≈ôiƒçten√≠ 127 k pozici MSB pro v√Ωpoƒçet exponentu
 
       ; Vypocet mantisy
       mov edx, 23       ; Nacteni 23 do registru edx - slouzi pro dopocteni nasledneho posunuti mantisy
@@ -55,6 +71,9 @@ main proc c
       shl esi, 23       ; Posun exponentu do spravne pozice
 
       ; Kombinace exponentu a mantisy
+      xor ebx, ebx
+      mov ebx, [sign]
+      or eax, ebx
       or eax, esi       ; Kombinace s ER4 (exponentem)
 
       ; Ulozeni vysledky do pameti
