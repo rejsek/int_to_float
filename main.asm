f914e58f2e359f5a930684d0f9b45629febeb3ac
Daniel <86194608+rejsek@users.noreply.github.com>
2023-08-09 08:08:41 +0200

    Update main.asm - updated output message

--- main.asm
+++ main.asm
@@ -20,37 +20,39 @@ ExitProcess PROTO, dwExitCode: DWORD
     consoleOutHandler dd ?
     consoleInHandler  dd ?
     result_digits     db 10 DUP(?)
+    
+    new_line          db " ", 10, 0
+    msg               db "Vysledek prevodu je: ", 13, 10, 0
 
     buffer          db	128 DUP(?)
     bytes	        dd  ?
 
 .code
 main proc c
-
-      INVOKE GetStdHandle, STD_INPUT_HANDLE             ; Vezmi input handler
-      mov consoleInHandler, eax
+    INVOKE GetStdHandle, STD_INPUT_HANDLE             ; Vezmi input handler
+    mov consoleInHandler, eax
   
-      ; Nacteni cisla z konzole
-      INVOKE ReadConsoleA, consoleInHandler, offset buffer, lengthof buffer, offset bytes, 0
+    ; Nacteni cisla z konzole
+    INVOKE ReadConsoleA, consoleInHandler, offset buffer, lengthof buffer, offset bytes, 0
 
-      mov eax, 0
-      mov esi, offset buffer
+    mov eax, 0
+    mov esi, offset buffer
 
-      movzx edx, byte ptr [esi]
-      cmp edx, 2Dh              ; Jestli je to "-"
-      jne convert_loop          ; Pokud neni vstup zaporny, pokracuj standartnim cyklem
+    movzx edx, byte ptr [esi]
+    cmp edx, 2Dh              ; Jestli je to "-"
+    jne convert_loop          ; Pokud neni vstup zaporny, pokracuj standartnim cyklem
 
-      inc esi                   ; Preskoc prvni znak
-      mov [sign], 1
+    inc esi                   ; Preskoc prvni znak
+    mov [sign], 1
 
-      ; Pretypovani na HEX cislo
-      convert_loop:
+    ; Pretypovani na HEX cislo
+    convert_loop:
 	    movzx edx, byte ptr [esi]   ; Načíst ASCII znak do EDX
 
 	    cmp edx, 0DH
 	    je done
 
-	    ; Provést konverzi na číslici a aktualizovat registru EAX
+	    ; Provést konverzi na číslici a aktualizovat registr EAX
         sub dl, '0'                ; Konverze ASCII na numerickou hodnotu
         imul eax, eax, 10          ; Násobení aktuálního výsledku deseti
         add eax, edx               ; Přidání aktuální číslice k výsledku
@@ -58,88 +60,90 @@ main proc c
 	    inc esi                    ; Posunout ukazatel na další znak v řetězci
 	    jmp convert_loop           ; Opakovat cyklus pro další znak
 
-      done:
-      mov [input], eax
+    done:
+        mov [input], eax
 
-      ; Inicializace
-      xor eax, eax ; eax - vynulování registru
-      xor ebx, ebx ; ebx - vynulování registru
-      xor ecx, ecx ; ecx - vynulování registru
-      xor edx, edx ; edx - vynulování registru
-      xor esi, esi ; esi - vynulování registru
+    ; Inicializace
+    xor eax, eax ; eax - vynulování registru
+    xor ebx, ebx ; ebx - vynulování registru
+    xor ecx, ecx ; ecx - vynulování registru
+    xor edx, edx ; edx - vynulování registru
+    xor esi, esi ; esi - vynulování registru
 
-      ; Nacteni vstupu
-      mov eax, input
+    ; Nacteni vstupu
+    mov eax, input
       
-      mov ebx, input
-      shr ebx, 31
+    mov ebx, input
+    shr ebx, 31
 
-      xor ebx, ebx      ; Vynulovani registru ebx
+    xor ebx, ebx      ; Vynulovani registru ebx
 
-      ; Hledani pozice nejvyznamnejsiho bitu (MSB)
-      mov ecx, 31           ; Inicializace čítače
-      mov edx, 80000000h    ; inicializave porovnavaci hodnoty
+    ; Hledani pozice nejvyznamnejsiho bitu (MSB)
+    mov ecx, 31           ; Inicializace čítače
+    mov edx, 80000000h    ; inicializave porovnavaci hodnoty
 
-      search_msb:
+    search_msb:
         cmp edx, eax    ; porovnani edx s eax
         jbe bit_found   ; pokud eax je mensi nebo rovno edx, skoc na nalezeny bit
         shr edx, 1      ; pousn edx doprava
         dec ecx         ; dekrementace citace
         jmp search_msb  ; opakovani cyklu
 
-      bit_found:
+    bit_found:
         mov [msb_pos], ecx  ; Ulozeni pozice MSB do pameti
 
-      ; Odstraneni nejvyznamnejsiho bitu
-      mov ebx, eax
-      btr ebx, ecx       ; Odstranění bitu na pozici MSB
-      mov eax, ebx
+    ; Odstraneni nejvyznamnejsiho bitu
+    mov ebx, eax
+    btr ebx, ecx       ; Odstranění bitu na pozici MSB
+    mov eax, ebx
 
-      ; Vypocet exponentu
-      mov esi, [msb_pos]    ; Načtení pozice MSB do registru ESI
-      add esi, 127          ; Přičtení 127 k pozici MSB pro výpočet exponentu
+    ; Vypocet exponentu
+    mov esi, [msb_pos]    ; Načtení pozice MSB do registru ESI
+    add esi, 127          ; Přičtení 127 k pozici MSB pro výpočet exponentu
 
-      ; Vypocet mantisy
-      mov edx, 23       ; Nacteni 23 do registru edx - slouzi pro dopocteni nasledneho posunuti mantisy
-      sub edx, ecx      ; Odecteni pozice hlavniho bitu od 23
-      mov ecx, edx      ; Presunuti obsahu registru edx do registru ecx
+    ; Vypocet mantisy
+    mov edx, 23       ; Nacteni 23 do registru edx - slouzi pro dopocteni nasledneho posunuti mantisy
+    sub edx, ecx      ; Odecteni pozice hlavniho bitu od 23
+    mov ecx, edx      ; Presunuti obsahu registru edx do registru ecx
 
-      shl eax, cl       ; Vezme low byte z registru ecx - shl pracuje pouze s konstantou nebo hodnotou ulozenou v cl
+    shl eax, cl       ; Vezme low byte z registru ecx - shl pracuje pouze s konstantou nebo hodnotou ulozenou v cl
 
-      ; Posun exponentu
-      shl esi, 23       ; Posun exponentu do spravne pozice
+    ; Posun exponentu
+    shl esi, 23       ; Posun exponentu do spravne pozice
 
-      ; Kombinace exponentu a mantisy
-      xor ebx, ebx      ; Vynulovani registru ebx
-      mov ebx, [sign]   ; Nacteni hodnoty v promenne sign
-      shl ebx, 31
-      or eax, ebx       ; Kombinace se znaminkem
-      or eax, esi       ; Kombinace s exponentem
+    ; Kombinace exponentu a mantisy
+    xor ebx, ebx      ; Vynulovani registru ebx
+    mov ebx, [sign]   ; Nacteni hodnoty v promenne sign
+    shl ebx, 31
+    or eax, ebx       ; Kombinace se znaminkem
+    or eax, esi       ; Kombinace s exponentem
 
-      ; Převod výsledku na hexadecimální řetězec
-      lea edi, result_digits + 8
-      mov ecx, 8
-      mov ebx, 16
+    ; Převod výsledku na hexadecimální řetězec
+    lea edi, result_digits + 8
+    mov ecx, 8
+    mov ebx, 16
 
-      convert_to_hex_loop:
-          mov edx, 0
-          div ebx
-          add dl, '0'           ; Pro zjisteni ASCII reprezentace cisla
-          cmp dl, '9'           ; Porovnani, jestli je hodnota '9' - pokud je vetsi, zvoli se pismeno
-          jbe not_letter
-          add dl, 7
+    convert_to_hex_loop:
+        mov edx, 0
+        div ebx
+        add dl, '0'           ; Pro zjisteni ASCII reprezentace cisla
+        cmp dl, '9'           ; Porovnani, jestli je hodnota '9' - pokud je vetsi, zvoli se pismeno
+        jbe not_letter
+        add dl, 7
 
-      not_letter:
-          mov [edi], dl
-          dec edi
-          loop convert_to_hex_loop
+    not_letter:
+        mov [edi], dl
+        dec edi
+        loop convert_to_hex_loop
 
-      mov byte ptr [edi], 0     ; Ukonceni retezce
+    mov byte ptr [edi], 0     ; Ukonceni retezce
 
-      INVOKE GetStdHandle, STD_OUTPUT_HANDLE
-      mov consoleOutHandler, eax
+    INVOKE GetStdHandle, STD_OUTPUT_HANDLE
+    mov consoleOutHandler, eax
 
-      INVOKE WriteConsoleA, consoleOutHandler, offset result_digits, 9, offset bytes, 0
+    INVOKE WriteConsoleA, consoleOutHandler, offset new_line, lengthof new_line, offset bytes, 0
+    INVOKE WriteConsoleA, consoleOutHandler, offset msg, lengthof msg, offset bytes, 0
+    INVOKE WriteConsoleA, consoleOutHandler, offset result_digits, 9, offset bytes, 0
 
     invoke  ExitProcess,0
 main endp
